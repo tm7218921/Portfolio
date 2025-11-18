a572494b6b662924a64b99ca07822ebc922c7861
Tanmay Mandal <142010031+tm7218921@users.noreply.github.com>
2025-11-18 15:43:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import React, { useEffect } from "react";
-import Header from "./components/Header";
-import ProfileSection from "./components/ProfileSection";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import WorkExperience from "./components/WorkExperience";
-import AcademicActivities from "./components/AcademicActivities";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import TechnicalBlog from "./components/TechnicalBlog";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
+// Pages
+import ProfileSection from "./pages/ProfileSection";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import WorkExperience from "./pages/WorkExperience";
+import AcademicActivities from "./pages/AcademicActivities";
+import WorkshopsAndHackathons from "./pages/WorkshopsAndHackathons";
+import Projects from "./pages/Projects";
+import TechnicalBlog from "./pages/TechnicalBlog";
+import Contact from "./pages/Contact";
 import { Helmet } from "react-helmet";
-
 import "./index.css";
-import WorkshopsAndHackathons from "./components/WorkshopsAndHackathons";
 
 function App() {
-  useEffect(() => {
-    document.title = "Tanmay Mandal - Portfolio";
-  }, []);
   return (
-    <>
-
+    <Router>
       <Helmet>
         <title>Tanmay Mandal - AI & Data Science Portfolio</title>
         <meta name="description" content="Portfolio of Tanmay Mandal - AI & Data Science Engineer" />
       </Helmet>
-
       <Header />
-      <ProfileSection />
-      <About />
-      <Skills />
-      <WorkExperience />
-      <WorkshopsAndHackathons />
-      <AcademicActivities />
-      <Projects />
-      <TechnicalBlog />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<ProfileSection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/work-experience" element={<WorkExperience />} />
+        <Route path="/workshops" element={<WorkshopsAndHackathons />} />
+        <Route path="/academic-activities" element={<AcademicActivities />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/blog" element={<TechnicalBlog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
-      
-    </>
+    </Router>
   );
 }
-
 export default App;
